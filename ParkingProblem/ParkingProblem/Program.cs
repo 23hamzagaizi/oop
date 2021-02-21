110ef06f8b28b1448eb9a145a5c0cb52e69fc90d
23hamzagaizi <hamza.gaizi@hotmail.com>
2021-02-21 19:25:45 +0000

    final (without automatic inputs)

---
+++ ParkingProblem/ParkingProblem/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ParkingProblem
+{
+    class Program
+    {
+        private readonly Random _random = new Random();
+        public int RandomNumber(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.Clear();
+            Console.WriteLine("Enter number <= 0 to exit\n");
+            while (true)
+            {
+                Console.Write(" Hours parked : ");
+                string entry = Console.ReadLine();
+                double hours;
+                //hours = RandomNumber(0, 24); i tried to simulate input but it doesnt work
+                double.TryParse(entry, out hours); // sets hours to 0 if entry is invalid
+                if (hours <= 0) return;
+                double charge = CalculateCharges(hours);
+                total += charge;
+                Console.WriteLine(" Charge is {0:C}, total today {1:C}\n", charge, total);
+            }
+        }
+        static double CalculateCharges(double hours)
+        {
+            if (hours <= 3) return 2;
+            double charge = 2 + 0.5 * Math.Ceiling(hours - 3);
+            if (charge > 10) charge = 10;
+            return charge;
+        }
+       
+        
+    }
+}
+
